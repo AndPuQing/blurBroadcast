1bb243cb8205d58a63499e6b82424710e0ee7f1b
AndPuqing <1569540783@qq.com>
2021-07-24 23:26:59 +0800

    some thing

--- src/components/introduce.jsx
+++ src/components/introduce.jsx
@@ -10,8 +10,8 @@ class Introduce extends Component {
                 <div className='text-xl font-bold text-white'>
                     I write interesting Bug. I am an alchemist from the magical of China.
                     Now studying at a&nbsp;
-                    <a className='underline' href='https://www.guet.edu.cn/' target='_blank'>
-                        <div className='border-2'>Gay Electronic Science and Technology University</div></a>
+                    <a className='bg-gradient-to-r from-yellow-200 to-yellow-200 bg-growing-underline' href='https://www.guet.edu.cn/' target='_blank'>
+                        Gay Electronic Science and Technology University</a>
                     .
                     This is my first open source project.There may be many questions.
 
