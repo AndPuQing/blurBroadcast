ce2c50b245d83c9d50e8c3a26de54c3127a648fd
AndPuqing <1569540783@qq.com>
2021-07-25 15:42:34 +0800

    fix githubAPI

--- src/components/repo.jsx
+++ src/components/repo.jsx
@@ -5,10 +5,10 @@ class Repo extends Component {
     render() {
         var data = this.props.data
         return (
-            <div className='h-20 m-2 rounded-lg bg-white shadow-md'>
-                <div className='font-bold pt-2 pl-2 text-lg truncate capitalize text-blue-500'>{data.name}</div>
-                <div className='font-bold pl-2 text-sm truncate capitalize text-blue-300'>{data.description}</div>
-
+            <div className='h-20 m-2 rounded-lg bg-white shadow-md pl-2 pt-2'>
+                <a href={data.html_url} target='_blank' className='font-bold pt-2 text-lg truncate capitalize bg-gradient-to-r from-blue-100 to-blue-100 bg-growing-underline'>{data.name}</a>
+                <div className='font-bold text-xs break-all capitalize text-blue-300'>{data.description}</div>
+                <div className='font-bold text-xs break-all capitalize text-black inline-block align-text-bottom'>{data.language}</div>
             </div>
         );
     }
--- src/components/reposlist.jsx
+++ src/components/reposlist.jsx
@@ -4,12 +4,15 @@ import Repo from './repo';
 class Reposlist extends Component {
     render() {
         return (
-            <div className='h-30 w-1/2 rounded-lg p-5 m-5 bg-gradient-to-tr from-green-400 to-indigo-300 shadow-lg grid grid-cols-2'>
-                {this.state.banners.map((element, index) => {
-                    return (
-                        <Repo key={index} data={element} />
-                    )
-                })}
+            <div className='h-30 w-1/2 rounded-lg p-5 m-5 bg-gradient-to-tr from-green-400 via-blue-200 to-pink-200 shadow-lg'>
+                <div className='font-bold text-2xl text-blue-700 m-2'>GitHub Project</div>
+                <div className='grid grid-cols-2'>
+                    {this.state.banners.map((element, index) => {
+                        return (
+                            <Repo key={index} data={element} />
+                        )
+                    })}
+                </div>
             </div>
         );
     }
@@ -17,7 +20,7 @@ class Reposlist extends Component {
         fetch("https://api.github.com/users/AndPuQing/repos")
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                // 读取前六个
                 console.log(data.slice(0, 6))
                 this.setState({ banners: data.slice(0, 6) })
             })
