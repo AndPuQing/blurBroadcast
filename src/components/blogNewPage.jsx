6334d7a80a31f290d736c8dc8db69ef8b8a90680
AndPuqing <1569540783@qq.com>
2021-07-31 14:57:29 +0800

    basically completed

--- src/components/blogNewPage.jsx
+++ src/components/blogNewPage.jsx
@@ -4,24 +4,28 @@ class BlogNewPage extends Component {
     render() {
         console.log(this.state)
         return (
-            <div className='h-20 ml-2 w-2/6 mr-5'>
-                <img id='blogimg' alt='load fail' src={require('../image/1627146470321.jpg').default} className=' w-full rounded-lg shadow-xl -z-1 bg_move'/>
+            <div onClick={() => { window.open(this.state.linkaddr, '_blank') }} className='h-20 ml-2 w-2/6 mr-5 overflow-hidden rounded-lg shadow-lg relative'>
+                <img id='blogimg' alt='loading fail' className=' w-full bg_move' />
+                <div className='text-white font-extrabold absolute text-4xl top-5 left-4 cursor-default'>最近文章</div>
             </div>
         );
     }
     componentDidMount() {
+        // 获取博客最新文章
         fetch("https://puqing.work/sitemap.xml")
             .then(response => {
                 response.text().then(text => {
                     var pattern = new RegExp('https://puqing.work/p/[^/]+/', 'g')
                     // console.log(pattern.exec(text)[0])
-                    this.setState({ imgsrc: pattern.exec(text)[0] + 'index.jpg' })
+                    var addr = pattern.exec(text)[0]
+                    this.setState({ linkaddr: addr })
+                    this.setState({ imgsrc: addr + 'index.jpg' })
                 })
             })
     }
     componentDidUpdate() {
         console.log(this.state.imgsrc)
-        // document.getElementById('blogimg').src=this.state.imgsrc
+        document.getElementById('blogimg').src = this.state.imgsrc
     }
 }
 
