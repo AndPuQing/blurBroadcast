cf9752c6f9f17d57ff8d3cc87fe901908f9629c3
AndPuqing <1569540783@qq.com>
2021-08-31 21:45:02 +0800

    fix error

--- src/components/copyright.jsx
+++ src/components/copyright.jsx
@@ -7,7 +7,7 @@ class Copyright extends Component {
                 <div className='h-1 bg-gray-500 w-20 rounded-lg mb-5'></div>
                 <p className='text-sm font-bold text-gray-600 cursor-default'>© 2020-2021 PuQing Site</p>
                 <div className='flex flex-row'>
-                    <a target='_blank' href='https://stats.uptimerobot.com/RKNGPSGkAY' className='text-sm font-semibold text-gray-300 cursor-default'>System State</a>
+                    <a rel="noreferrer" target='_blank' href='https://stats.uptimerobot.com/RKNGPSGkAY' className='text-sm font-semibold text-gray-300 cursor-default'>System State</a>
                     <span className="animate-ping duration-500 to-gray-50 transition ml-2 self-center rounded-full h-2 w-2 bg-green-500"></span>
                 </div>
                 <p className='text-xs font-semibold text-gray-300 mt-3 cursor-default'>Built with React</p>
